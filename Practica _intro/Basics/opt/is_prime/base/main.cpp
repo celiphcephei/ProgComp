a8116070752357ebecb9c4c1267560ffd93939c0
A01799387 <a01799387@tec.mx>
2023-03-20 22:49:17 -0600

    optimizacion

---
+++ Practica _intro/Basics/opt/is_prime/base/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+bool is_prime(int n);
+
+
+int main(){
+    int n;
+    cin >> n;
+    bool prime = is_prime(n);
+    if(prime == true){
+        cout << n << " es primo";
+    } else {
+        cout << n << " no es primo";
+    }
+    return 0;
+}
+
+
+bool is_prime(int n){
+    for(int i = 2; i * i <= n; i++){
+        if(n % i == 0) return false;
+    }
+    return true;
+}
---
+++ Practica _intro/Basics/opt/is_prime/medio/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+bool is_prime(int n);
+
+
+int main(){
+    int n;
+    cin >> n;
+    bool prime = is_prime(n);
+    if(prime == true){
+        cout << n << " es primo";
+    } else {
+        cout << n << " no es primo";
+    }
+    return 0;
+}
+
+
+bool is_prime(int n){
+    if(n % 2 == 0 and n > 2) return false;
+    for(int i = 3; i < n; i += 2){
+        if(n % i == 0) return false;
+    }
+    return true;
+}
