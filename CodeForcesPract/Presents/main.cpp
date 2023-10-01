3f370a495965f9637a43132c748301184e200dcf
Iris14159 <ampheriz0@gmail.com>
2023-09-30 21:08:49 -0600

    -

--- CodeForcesPract/Presents/main.cpp
+++ CodeForcesPract/Presents/main.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main() {
+    int n; scanf("%d", &n);
+    vector<int> friends(n);
+    for (int i = 0; i < n; ++i)
+        scanf("%d", &friends[i]);
+    
+    vector<int> result(n);
+    for (int i = 0; i < n; ++i)
+        result[friends[i] - 1] = i + 1;
+
+    for (int i = 0; i < n; ++i)
+        printf("%d ", result[i]);
 
     return 0;
 }
