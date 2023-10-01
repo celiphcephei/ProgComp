1e042fc97fb92eef8df6b02352989d39d5348e63
Iris14159 <ampheriz0@gmail.com>
2023-10-01 15:05:19 -0600

    -

--- CodeForcesPract/Magnets/main.cpp
+++ CodeForcesPract/Magnets/main.cpp
@@ -1,7 +1,18 @@
+// Author: RenatoGM
 #include <bits/stdc++.h>
 using namespace std;
+// C cosas
 int main(){
     ios::sync_with_stdio(0);
-    int n;
+    cin.tie(nullptr);
+    int n, ans{1}; cin >> n;
+    string current, prev{"--"};
+    for(int i{0}; i < n; ++i){
+        cin >> current;
+        if(current[0] == prev[1]) ++ans;
+        prev = current;
+    }
+    cout << ans << '\n';
+    cout.flush();
     return 0;
 }
