0abd59b77ab03eb423256c427b5885307fa6f951
Iris14159 <ampheriz0@gmail.com>
2023-10-24 13:35:15 -0600

    -

--- CodeForcesPract/main/MediumNumber/main.cpp
+++ CodeForcesPract/main/MediumNumber/main.cpp
@@ -5,7 +5,15 @@ using namespace std;
 #define pb(x) push_back(x)
 #define debug(x) cout << #x << " = " << x << endl;
 
+void solve(){
+  vector<int> nums(3);
+  for(int i{0}; i < 3; ++i) scanf("%d", &nums[i]);
+  sort(ALL(nums));
+  printf("%d\n", nums[1]);
+}
+
 int main(){
-  
+  int t; scanf("%d", &t);
+  while(t--) solve();
   return 0;
 }
